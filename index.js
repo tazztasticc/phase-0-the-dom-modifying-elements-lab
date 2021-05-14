0da25fc23bc3b2fb2e8afdde09aed6e69d4c269c
Tasif <taz181995@gmail.com>
2021-05-14 09:19:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const body= document.getElementsByTagName("body")[0];
+body.removeChild(body.querySelector("main"));
+
+const newHeader = document.createElement('newHeader');
+document.querySelector('main').remove();
+const newHeader = document.createElement('h1');
+document.newHeader.appendChild('h1');
+h1.id='victory';
